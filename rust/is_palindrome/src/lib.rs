b25cc1d316516154112fe62a072546b2e81080d3
ydnan <nandygullejr@yahoo.com>
2024-04-26 21:09:01 +0800

    [rust] Create starter code for 'Palindrome Number' problem.

---
+++ rust/is_palindrome/src/lib.rs
@@ -0,0 +1,56 @@
+/*
+9. Palindrome Number
+Easy
+Topics
+Companies
+Hint
+Given an integer x, return true if x is a
+palindrome
+, and false otherwise.
+
+
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+
+Constraints:
+
+-231 <= x <= 231 - 1
+
+Follow up: Could you solve it without converting the integer to a string?
+*/
+struct Solution {}
+impl Solution {
+    pub fn is_palindrome(x: i32) -> bool {
+        false
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = Solution::is_palindrome(121);
+        assert_eq!(result, true);
+        let result = Solution::is_palindrome(-121);
+        assert_eq!(result, false);
+        let result = Solution::is_palindrome(10);
+        assert_eq!(result, false);
+    }
+}
