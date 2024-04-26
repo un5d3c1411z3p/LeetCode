712fb0b2fe2dc80b8e8334f6b5ffd52a8034aece
ydnan <nandygullejr@yahoo.com>
2024-04-26 21:57:37 +0800

    [rust] Refactor reverse_digit() to process any values within i32.

--- rust/is_palindrome/src/lib.rs
+++ rust/is_palindrome/src/lib.rs
@@ -49,7 +49,15 @@ impl Solution {
     }
 
     fn reverse_digits(x: i32) -> i32 {
-        121
+        let mut result = 0;
+        let mut copy = x;
+        while copy > 9 {
+            result = result + (copy % 10);
+            result *= 10;
+            copy /= 10;
+        }
+        result += copy;
+        result
     }
 }
 
