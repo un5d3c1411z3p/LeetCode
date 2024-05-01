ee363c34133ad752e271ed0257219f5259bbb76f
ydnan <nandygullejr@yahoo.com>
2024-05-01 19:43:12 +0800

    [rust] Refactor to starter code for 'Longest Common Prefix' algorithm problem in Rust.

--- rust/longest_common_prefix/src/lib.rs
+++ rust/longest_common_prefix/src/lib.rs
@@ -1,5 +1,36 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/*
+14. Longest Common Prefix
+Easy
+Topics
+Companies
+Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+ */
+struct Solution {}
+impl Solution {
+    pub fn longest_common_prefix(strs: Vec<String>) -> String {
+        "".to_string()
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +39,17 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = Solution::longest_common_prefix(vec![
+            "flower".to_string(),
+            "flow".to_string(),
+            "flight".to_string(),
+        ]);
+        assert_eq!(result, "fl".to_string());
+        let result = Solution::longest_common_prefix(vec![
+            "dog".to_string(),
+            "racecar".to_string(),
+            "car".to_string(),
+        ]);
+        assert_eq!(result, "".to_string());
     }
 }
