fd65c8006fc3ddfbc7a39b7d02faefd62387d3ce
ydnan <nandygullejr@yahoo.com>
2024-05-19 17:23:00 +0800

    Update to 'Merge Two Sorted Lists' algorithm problem starter code.

--- rust/merge_two_lists/src/lib.rs
+++ rust/merge_two_lists/src/lib.rs
@@ -1,5 +1,60 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/*
+21. Merge Two Sorted Lists
+Easy
+Topics
+Companies
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list.
+
+
+
+Example 1:
+
+
+Input: list1 = [1,2,4], list2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+Example 2:
+
+Input: list1 = [], list2 = []
+Output: []
+Example 3:
+
+Input: list1 = [], list2 = [0]
+Output: [0]
+
+
+Constraints:
+
+The number of nodes in both lists is in the range [0, 50].
+-100 <= Node.val <= 100
+Both list1 and list2 are sorted in non-decreasing order.
+ */
+
+// Definition for singly-linked list.
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+struct Solution {}
+impl Solution {
+    pub fn merge_two_lists(
+        list1: Option<Box<ListNode>>,
+        list2: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        None
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +63,49 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let list1 = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 2,
+                next: Some(Box::new(ListNode { val: 4, next: None })),
+            })),
+        }));
+        let list2 = Some(Box::new(ListNode {
+            val: 1,
+            next: Some(Box::new(ListNode {
+                val: 3,
+                next: Some(Box::new(ListNode { val: 4, next: None })),
+            })),
+        }));
+        let result = Solution::merge_two_lists(list1, list2);
+        assert_eq!(
+            result,
+            Some(Box::new(ListNode {
+                val: 1,
+                next: Some(Box::new(ListNode {
+                    val: 1,
+                    next: Some(Box::new(ListNode {
+                        val: 2,
+                        next: Some(Box::new(ListNode {
+                            val: 3,
+                            next: Some(Box::new(ListNode {
+                                val: 4,
+                                next: Some(Box::new(ListNode { val: 4, next: None }))
+                            }))
+                        }))
+                    }))
+                }))
+            }))
+        );
+
+        let list1 = None;
+        let list2 = None;
+        let result = Solution::merge_two_lists(list1, list2);
+        assert_eq!(result, None);
+
+        let list1 = None;
+        let list2 = Some(Box::new(ListNode { val: 0, next: None }));
+        let result = Solution::merge_two_lists(list1, list2);
+        assert_eq!(result, Some(Box::new(ListNode { val: 0, next: None })));
     }
 }
