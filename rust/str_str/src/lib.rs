492407e014a3c4587f30eb5a461926ec76f580e6
ydnan <nandygullejr@yahoo.com>
2024-06-12 08:42:31 +0800

    Create starter code for 'Find the Index of the First Occurrence in a String' algorithm problem'.

--- rust/str_str/src/lib.rs
+++ rust/str_str/src/lib.rs
@@ -1,5 +1,33 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/*
+28. Find the Index of the First Occurrence in a String
+
+Given two strings needle and haystack, return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+
+Example 1:
+
+Input: haystack = "sadbutsad", needle = "sad"
+Output: 0
+Explanation: "sad" occurs at index 0 and 6.
+The first occurrence is at index 0, so we return 0.
+Example 2:
+
+Input: haystack = "leetcode", needle = "leeto"
+Output: -1
+Explanation: "leeto" did not occur in "leetcode", so we return -1.
+
+
+Constraints:
+
+1 <= haystack.length, needle.length <= 104
+haystack and needle consist of only lowercase English characters.
+ */
+struct Solution {}
+impl Solution {
+    pub fn str_str(haystack: String, needle: String) -> i32 {
+        -1
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +36,10 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = Solution::str_str("sadbutsad".to_owned(), "sad".to_owned());
+        assert_eq!(result, 0);
+
+        let result = Solution::str_str("leetcode".to_owned(), "leeto".to_owned());
+        assert_eq!(result, -1);
     }
 }
