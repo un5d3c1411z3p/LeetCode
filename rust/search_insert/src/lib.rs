5a41de651b7e10ad1c72b6d3ab22f3ec02624821
ydnan <nandygullejr@yahoo.com>
2024-06-18 21:11:30 +0800

    Revise to starter code for the 'Search Insert Position' algorithm problem.

--- rust/search_insert/src/lib.rs
+++ rust/search_insert/src/lib.rs
@@ -1,5 +1,37 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/*
+35. Search Insert Position
+Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+
+Example 1:
+
+Input: nums = [1,3,5,6], target = 5
+Output: 2
+Example 2:
+
+Input: nums = [1,3,5,6], target = 2
+Output: 1
+Example 3:
+
+Input: nums = [1,3,5,6], target = 7
+Output: 4
+
+
+Constraints:
+
+1 <= nums.length <= 104
+-104 <= nums[i] <= 104
+nums contains distinct values sorted in ascending order.
+-104 <= target <= 104
+ */
+struct Solution {}
+impl Solution {
+    pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
+        0
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +40,11 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
+        let result = Solution::search_insert([1, 3, 5, 6].to_vec(), 5);
+        assert_eq!(result, 2);
+        let result = Solution::search_insert([1, 3, 5, 6].to_vec(), 2);
+        assert_eq!(result, 1);
+        let result = Solution::search_insert([1, 3, 5, 6].to_vec(), 7);
         assert_eq!(result, 4);
     }
 }
