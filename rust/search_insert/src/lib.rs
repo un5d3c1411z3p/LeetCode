d6a846f8ee82ea3331d3d1e012a7477fde3e4a86
ydnan <nandygullejr@yahoo.com>
2024-06-18 21:40:49 +0800

    Refactor to simplify the 1-liner code.

--- rust/search_insert/src/lib.rs
+++ rust/search_insert/src/lib.rs
@@ -30,7 +30,7 @@ nums contains distinct values sorted in ascending order.
 struct Solution {}
 impl Solution {
     pub fn search_insert(nums: Vec<i32>, target: i32) -> i32 {
-        nums.binary_search(&target).unwrap_or_else(|x| x) as i32
+        nums.partition_point(|&nums| nums < target) as i32
     }
 }
 
