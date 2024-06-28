7d3f9de4a8ae5ce4a1b24bb4d1ca4b0382298a14
ydnan <nandygullejr@yahoo.com>
2024-06-28 10:41:33 +0800

    Revise to 'Add Binary' algorithm problem starter code.

--- rust/add_binary/src/lib.rs
+++ rust/add_binary/src/lib.rs
@@ -1,5 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/*
+67. Add Binary
+Easy
+Given two binary strings a and b, return their sum as a binary string.
+
+
+
+Example 1:
+
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+
+Input: a = "1010", b = "1011"
+Output: "10101"
+
+
+Constraints:
+
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+ */
+struct Solution {}
+impl Solution {
+    pub fn add_binary(a: String, b: String) -> String {
+        "".to_string()
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +34,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = Solution::add_binary("11".to_string(), "1".to_string());
+        assert_eq!(result, "100".to_string());
+        let result = Solution::add_binary("1010".to_string(), "1011".to_string());
+        assert_eq!(result, "10101".to_string());
     }
 }
