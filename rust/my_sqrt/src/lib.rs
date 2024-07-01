101165a7584d50d50627157932d5fa2ea2e2c027
ydnan <nandygullejr@yahoo.com>
2024-07-01 20:24:44 +0800

    Revise to 'Sqrt(x)' algorithm problem starter code.

--- rust/my_sqrt/src/lib.rs
+++ rust/my_sqrt/src/lib.rs
@@ -1,5 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+/*
+69. Sqrt(x)
+Easy
+Hint
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+
+Constraints:
+
+0 <= x <= 231 - 1
+ */
+struct Solution {}
+impl Solution {
+    pub fn my_sqrt(x: i32) -> i32 {
+        0
+    }
 }
 
 #[cfg(test)]
@@ -8,7 +38,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        let result = Solution::my_sqrt(4);
+        assert_eq!(result, 2);
+        let result = Solution::my_sqrt(8);
+        assert_eq!(result, 2);
     }
 }
