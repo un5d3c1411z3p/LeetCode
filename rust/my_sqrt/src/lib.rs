9f015744397d36c9b42d39ea54449620256e57c7
ydnan <nandygullejr@yahoo.com>
2024-07-01 21:18:33 +0800

    Refactor based on MinnDev's solution.

--- rust/my_sqrt/src/lib.rs
+++ rust/my_sqrt/src/lib.rs
@@ -25,18 +25,50 @@ Constraints:
 
 0 <= x <= 231 - 1
  */
+use std::cmp::Ordering;
+
 struct Solution {}
 impl Solution {
     pub fn my_sqrt(x: i32) -> i32 {
-        let mut result = 0;
-        for i in 1..=u32::MAX {
-            if i * i > x as u32 {
-                break;
-            }
-            result = i;
-            println!("result is {result}");
+        if x == 0 {
+            // non-negative => 0 is allowed
+            return 0;
         }
-        result as i32
+
+        let mut lower = 2;
+        let mut higher = 46340.min(x / 2); // max possible root sqrt(i32::MAX)
+
+        // Boundary conditions for optimization
+
+        if x <= 3 {
+            return 1;
+        }
+
+        if x >= higher * higher {
+            return higher;
+        }
+
+        // Binary search the correct perfect square
+
+        while higher - lower > 1 {
+            let mid = (higher + lower) / 2;
+            let pow = mid * mid;
+            match pow.cmp(&x) {
+                Ordering::Less => {
+                    lower = mid;
+                }
+                Ordering::Greater => {
+                    higher = mid;
+                }
+                _ => {
+                    return mid;
+                }
+            }
+        }
+
+        // In the edge case that the number is between 2 values,
+        // we take the lower which is equivalent to taking the floored mean
+        lower
     }
 }
 
